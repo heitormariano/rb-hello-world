f2f4ef6589a5a40b6ee34ac3c173183384eac339
heitormariano <heitor.eumesmo@gmail.com>
2022-09-01 13:42:20 -0300

    correcao de valores usados em testes

--- aula-05/teste.js
+++ aula-05/teste.js
@@ -1,3 +1,3 @@
-console.log(janeiro.totalizador.saldo === 408.5);
-console.log(fevereiro.totalizador.saldo === -45.65);
-console.log(marco.totalizador.saldo === -435.22);
+console.log(janeiro.totalizador.saldo === 451.5);
+console.log(fevereiro.totalizador.saldo === 1.58);
+console.log(marco.totalizador.saldo === -383.26);
